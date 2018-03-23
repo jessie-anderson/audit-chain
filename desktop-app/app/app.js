3e0e53b9ca8a33ff6d2466da6242aedddfc0365d
Jessie Anderson <Jessie.L.Anderson.18@dartmouth.edu>
2018-03-23 14:14:08 -0400

    boilerplate desktop app

--- desktop-app/app/app.js
+++ desktop-app/app/app.js
@@ -1,1 +1,38 @@
-import { app, browserWindow } from 'electron'
+import { app, BrowserWindow } from 'electron'
+import path from 'path'
+import url from 'url'
+
+function createWindow() {
+  let win = new BrowserWindow({
+    width: 800,
+    height: 800,
+  })
+
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true,
+  }))
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
