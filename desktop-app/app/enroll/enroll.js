7f74e05c1797bfe431a2dbd1baccb78777b3762f
Jessie Anderson <Jessie.L.Anderson.18@dartmouth.edu>
2018-05-09 19:51:00 -0400

    enrollment page styling updated

--- desktop-app/app/enroll/enroll.js
+++ desktop-app/app/enroll/enroll.js
@@ -3,7 +3,8 @@ import path from 'path'
 import enroll from '../api/enroll'
 import { loadPage } from '../lib/electron-helpers'
 
-$('#enroll-button').click(() => {
+$('#enroll-user-form').submit(() => {
+  console.log('submit')
   const username1 = $('#username-1').val()
   const username2 = $('#username-2').val()
   const password1 = $('#password-1').val()
@@ -27,4 +28,31 @@ $('#enroll-button').click(() => {
       $('#request-error').text(`Error: ${errResponse.statusText}`)
     })
   }
+  return false
 })
+
+// $('#enroll-button').click(() => {
+//   const username1 = $('#username-1').val()
+//   const username2 = $('#username-2').val()
+//   const password1 = $('#password-1').val()
+//   const password2 = $('#password-2').val()
+//
+//   if (username1 !== username2) {
+//     $('#username-error').text('Usernames do not match')
+//   }
+//   if (password1 !== password2) {
+//     $('#password-error').text('Passwords do not match')
+//   }
+//
+//   if (username1 === username2 && password1 === password2) {
+//     enroll(username1, password1)
+//     .then((data) => {
+//       localStorage.setItem('user', JSON.stringify(data.user))
+//       localStorage.setItem('token', data.token)
+//       loadPage(path.join(__dirname, '../main/main.jade'))
+//     })
+//     .catch((errResponse) => {
+//       $('#request-error').text(`Error: ${errResponse.statusText}`)
+//     })
+//   }
+// })
